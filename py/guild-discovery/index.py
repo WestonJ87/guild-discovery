5ad24dbea0633feb29eb71f0a736d95186e196f8
Scozzi <WestonJones87@gmail.com>
2020-11-22 01:26:40 -0500

    tinking with host #

--- py/guild-discovery/index.py
+++ py/guild-discovery/index.py
@@ -19,7 +19,7 @@ port = 5000
 initialize_db(app)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 @app.cli.command()
 def refreshGuilds():
